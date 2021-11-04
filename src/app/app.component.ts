b0f8ebf999d23126379d4ae7eb3d13cf4d361d15
Megha Mahesh <megha@google.com>
2021-11-04 23:40:05 +0530

    inital changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent {
+  title = 'megha-tut ' + environment.secret;
+}
